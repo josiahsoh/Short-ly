48eb13171d3d3728f2cd35a7cff7fd35b10fd723
Josiah <josiahsoh@gmail.com>
2015-12-01 09:10:35 -0800

    Successfully Shorten URL

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,15 @@
 get '/' do
+  @urls = Url.all
   erb :"static/index"
 end
+
+post '/urls' do
+  url = Url.create(long_urls: params[:long_url]) 
+  redirect to "/" 
+end
+
+get '/:short_url' do
+  url = Url.find_by(short_urls: params[:long_urls])
+  url.save #if url.create means create and save automatically.
+end
+
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,8 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	before_create :shorten 
+
+	def shorten
+	  self.short_urls = SecureRandom.hex(6)
+	end
 end
